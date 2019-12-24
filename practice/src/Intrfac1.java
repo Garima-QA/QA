70ea6d1ea39ba1a5e18abb4f4cfacefd43dea87f
Garima Gulati <Garima Gulati@DESKTOP-8AND331>
2019-12-23 20:56:44 -0500

    Intrface

---
+++ practice/src/Intrfac1.java
@@ -0,0 +1,27 @@
+
+
+interface Intrfac1 {
+ void show();
+}
+interface Intrfac2{
+	void display();
+}
+
+class Tree1 implements Intrfac1, Intrfac2{
+	public void show() {
+		System.out.println("Hello");
+	}
+	
+	
+	public void display() {
+		System.out.println("Hello1");
+	}
+	
+	public static void main(String[] args) {
+		Tree1 t2= new Tree1();
+		t2.show();
+		t2.display();
+	}
+	
+	
+}
