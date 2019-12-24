94ae5335753155b2aec90af4b1b462349056b14c
Garima Gulati <Garima Gulati@DESKTOP-8AND331>
2019-12-23 21:02:17 -0500

    Interface

---
+++ practice/src/Intrfac.java
@@ -0,0 +1,16 @@
+
+
+
+interface Intrfac {
+ void show();
+ }
+class Tree implements Intrfac{
+	public void show() {
+		System.out.println("Hello");
+	}
+public static void main(String[] args) {
+	Tree t= new Tree();
+	t.show();
+}
+	
+}
