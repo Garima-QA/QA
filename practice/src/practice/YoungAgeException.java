ecabf75bf29a5c97b42f36c9bd464040341da30c
Garima Gulati <Garima Gulati@192.168.0.14>
2019-12-28 13:33:51 -0500

    User-defined Exception

---
+++ practice/src/practice/YoungAgeException.java
@@ -0,0 +1,23 @@
+package practice;
+//Customised unchecked exception using throw keyword.
+
+class YoungAgeException extends RuntimeException {
+	YoungAgeException( String msg){
+	   
+	    }
+}
+
+class Voting{
+	
+	public static void main(String[] args) {
+		int age= 16;
+		
+		if(age<18) {
+			throw new YoungAgeException("You are not eligible to vote");
+		} else {
+			System.out.println("Please vote");
+		}
+
+	}
+
+}
